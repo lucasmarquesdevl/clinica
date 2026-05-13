3629e15ed4903f7233d86c9e242f5ed017bd51d1
Lucas Trindade Marques <lucaas.marquees22@gmail.com>
2026-05-13 13:38:32 -0300

    atualizações

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ====== CONFIGURAÇÃO ======
 const SUPABASE_URL = 'https://ezutgtfynlvbgbqmpqyb.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6dXRndGZ5bmx2YmdicW1wcXliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NDMyODcsImV4cCI6MjA5NDIxOTI4N30.t1VcyLhhfe8n_l_YDUgkx6u-YGm_PpDG7lALE52loaE';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6dXRndGZ5bmx2YmdicW1wcXliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NDMyODcsImV4cCI6MjA5NDIxOTI4N30.' + 't1VcyLhhfe8n_l_YDUgkx6u-YGm_PpDG7lALE52loaE';
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // O USERS agora é opcional, pois usaremos o Supabase Auth para gerenciar os IDs e senhas.
